59ff210981ae7ae13e4d025af955934849d904e8
阳君 <937447974@qq.com>
2019-03-19 10:04:54 +0800

    no message

--- Cocoa/AppFrameworks/Foundation/URLRouter/Node/YJNSRouterRegister.h
+++ Cocoa/AppFrameworks/Foundation/URLRouter/Node/YJNSRouterRegister.h
@@ -23,13 +23,13 @@ NS_ASSUME_NONNULL_BEGIN
 /**
  *  @abstract 初始化
  *
- *  @param cls   节点实现类
- *  @param url   节点对应的路径
+ *  @param url YJNSRouterURL
  *  @param cache 是否缓存
+ *  @param cls   节点YJNSURLRouterProtocol实现类
  *
  *  @return instancetype
  */
-- (instancetype)initWithClass:(Class)cls url:(NSString *)url cache:(BOOL)cache;
+- (instancetype)initWithURL:(NSString *)url cache:(BOOL)cache cls:(Class)cls;
 
 /**
  *  @abstract 初始化
--- Cocoa/AppFrameworks/Foundation/URLRouter/Node/YJNSRouterRegister.m
+++ Cocoa/AppFrameworks/Foundation/URLRouter/Node/YJNSRouterRegister.m
@@ -10,7 +10,7 @@
 
 @implementation YJNSRouterRegister
 
-- (instancetype)initWithClass:(Class)cls url:(NSString *)url cache:(BOOL)cache {
+- (instancetype)initWithURL:(NSString *)url cache:(BOOL)cache cls:(Class)cls {
     self = [super init];
     if (self) {
         self.url = url;
